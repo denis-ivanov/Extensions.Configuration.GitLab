1ee72f7eb6f82fb5dea66f0171280d91b5fd61e5
Denis Ivanov <denisvictorovichivanov@gmail.com>
2026-02-17 15:12:03 +0200

    Release 2.0.0

--- Extensions.Configuration.GitLab/GitLabConfigurationSource.cs
+++ Extensions.Configuration.GitLab/GitLabConfigurationSource.cs
@@ -3,24 +3,23 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.Diagnostics.CodeAnalysis;
 
-namespace Extensions.Configuration.GitLab
+namespace Extensions.Configuration.GitLab;
+
+public class GitLabConfigurationSource : IConfigurationSource
 {
-    public class GitLabConfigurationSource : IConfigurationSource
+    private readonly IGitLabClient _gitLabClient;
+    private readonly GitLabConfigurationOptions _options;
+
+    public GitLabConfigurationSource(
+        [NotNull] IGitLabClient gitLabClient,
+        [NotNull] GitLabConfigurationOptions options)
     {
-        private readonly IGitLabClient _gitLabClient;
-        private readonly GitLabConfigurationOptions _options;
+        _gitLabClient = gitLabClient ?? throw new ArgumentNullException(nameof(gitLabClient));
+        _options = options ?? throw new ArgumentNullException(nameof(options));
+    }
 
-        public GitLabConfigurationSource(
-            [NotNull] IGitLabClient gitLabClient,
-            [NotNull] GitLabConfigurationOptions options)
-        {
-            _gitLabClient = gitLabClient ?? throw new ArgumentNullException(nameof(gitLabClient));
-            _options = options ?? throw new ArgumentNullException(nameof(options));
-        }
-
-        public IConfigurationProvider Build(IConfigurationBuilder builder)
-        {
-            return new GitLabConfigurationProvider(_gitLabClient, _options);
-        }
+    public IConfigurationProvider Build(IConfigurationBuilder builder)
+    {
+        return new GitLabConfigurationProvider(_gitLabClient, _options);
     }
 }
